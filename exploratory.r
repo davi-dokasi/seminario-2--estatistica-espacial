c5e0e2749a5b96d13a09092fc8abba63be03c5f6
davi-dokasi <daviaugusto2509@gmail.com>
2025-12-09 10:15:10 -0300

    third commit

--- exploratory.r
+++ exploratory.r
@@ -1,6 +1,7 @@
 library(geoR)
 
 data(parana)
+
 View(parana)
 summary(parana)
 
@@ -12,3 +13,15 @@ View(parana$data)
 hist(parana$data, main = "Histograma dos Dados", col = "lightblue")
 
 
+tabela <- data.frame(
+  ID = 1:3,
+  X = round(parana$coords[1:3, 1], 1), 
+  Y = round(parana$coords[1:3, 2], 1),
+  Chuva = parana$data[1:3]
+)
+tabela_qmd <- rbind(as.matrix(tabela), c("...", "...", "...", "..."))
+
+# Gera a tabela no estilo Markdown
+knitr::kable(tabela_qmd, 
+             col.names = c("ID", "Coord X", "Coord Y", "Precipitação Média"),
+             align = "c")
